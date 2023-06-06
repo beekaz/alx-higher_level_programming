841e69dfb598131b1a4eb5b675da623f89f81b34
beekaz <sooperfect00@gmail.com>
2023-06-06 15:15:34 -0700

    changed the variable for the if/else

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,10 +3,9 @@ import random
 number = random.randint(-10000, 10000)
 number = str(number)
 last_digit = int(number[-1])
-number = int(number)
-if number > 5:
-    print(f" last dight of {number} is {last_digit} and is greater than 5")
-elif number == 0:
-    print(f"last dight of {number} is {last_digit} is 0 and is zero")
-elif number  < 6 != 0:
-    print(f"last digit of {number} is {last_digit} and is less than 6 and not 0")
+if last_digit > 5:
+    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+elif last_digit == 0:
+    print(f"Last digit of {number} is {last_digit} and is 0")
+elif last_digit < 6 and last_digit != 0:
+    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
