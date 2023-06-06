de11a02429114d739222223cfdad822bfbfc1717
beekaz <sooperfect00@gmail.com>
2023-06-06 16:17:36 -0700

    reviewed the conditions to restate whether the number is 0, positive, or negative.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-number = str(number)
-last_digit = int(number[-1])
-if last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
-elif last_digit == 0:
-    print(f"Last digit of {number} is {last_digit} and is 0")
-elif last_digit < 6 and last_digit != 0:
-    print(f"Last digit of {number} is -{last_digit} and is less than 6 and not 0")
+if number == 0:
+    last = 0
+elif number > 0:
+    last = int(str(number)[-1])
+else:
+    last = -1 * (int(str(number)[-1]))
+if last > 5:
+    print(f"Last digit of {number} is {last} and is greater than 5")
+elif last == 0:
+    print(f"Last digit of {+number} is {last} and is 0")
+elif last < 6 and last != 0:
+    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
