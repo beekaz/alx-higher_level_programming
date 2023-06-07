b17830e3150ebe7cd43a93ba7bfadff57edd4eef
beekaz <sooperfect00@gmail.com>
2023-06-06 17:05:30 -0700

    resolved indentation errors

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for ascii_numbers in range(97, 123):
-     print("{alphabet}".format(alphabet=chr(ascii_numbers)), end="")
+    print("{alphabet}".format(alphabet=chr(ascii_numbers)), end="")
