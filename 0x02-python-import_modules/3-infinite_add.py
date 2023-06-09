fcd730acd39fd7e7638e28acc46153465ed5651e
beekaz <sooperfect00@gmail.com>
2023-06-08 19:56:00 -0700

    added a line of code that calculates the number of command-line arguments passed to a script and assigns the result to the variable argc

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 from sys import argv
-
+argc = len(argv) - 1
 
 def sum_args():
     sum = 0
     for i in range(1, len(argv)):
         sum += int(argv[i])
     return sum
-
 
 if __name__ == "__main__":
     if (argc == 0):
