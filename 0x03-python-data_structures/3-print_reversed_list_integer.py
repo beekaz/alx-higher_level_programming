af5c78b457bdf7a3a81a4f2d8d96b71f6837e4fe
beekaz <sooperfect00@gmail.com>
2023-06-10 16:44:46 -0700

    used str.format(int) to print integers instead of the normal print()

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@ def print_reversed_list_integer(my_list=[]):
         my_list.sort()
         my_list.reverse()
         for integers in my_list:
-            print(integers)
+            print("{:d}".format(int(integers)))
