f6235345f1f185d09e98a7ffc7e43c18248655e4
beekaz <sooperfect00@gmail.com>
2023-06-12 15:23:51 +0000

    added blank lines after class or function definition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,15 +3,14 @@ import sys
 
 argc = len(sys.argv) - 1
 
-
 def sum_args():
-    sum = 0
+    total_sum = 0
     for i in range(1, len(sys.argv)):
-        sum += int(sys.argv[i])
-    return sum
+        total_sum += int(sys.argv[i])
+    return total_sum
 
-if __name__ == "__main__":
-    if (argc == 0):
+if _name_ == "_main_":
+    if argc == 0:
         print("{:d}".format(argc))
     else:
         print("{:d}".format(sum_args()))
