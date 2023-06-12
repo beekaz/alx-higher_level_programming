67815814e8d53eb5c4669b68c4a8393069224b3e
Beekaz <sooperfect00@gmail.com>
2023-06-12 01:53:50 +0100

    Delete 0x03-python-data_structures directory
    directory with the wrong configuration

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    my_list.sort()
-    if my_list:
-        my_list.reverse()
-        for element in my_list:
-            print("{:d}".format(element))
--- 0x03-python-data_structures/5-no_c.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-def no_c(my_string):
-    my_string = my_string.replace('c', '')
-    my_string = my_string.replace('C', '')
-    return my_string
