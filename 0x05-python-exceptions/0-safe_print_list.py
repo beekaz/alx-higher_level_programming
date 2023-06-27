c1ecee53204fcc2803e2906af8c5340d48200bab
beekaz <sooperfect00@gmail.com>
2023-06-26 18:57:43 -0700

    format

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
     number = 0
     try:
-        for element in range(x):
-            print("{}".format(my_list[element]), end="")
+        for index in range(x):
+            print(my_list[index], end='')
             number += 1
     except IndexError:
-        print(" ")
-        return number
+        None
+    print()
+    return number
