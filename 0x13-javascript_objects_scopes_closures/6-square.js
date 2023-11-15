288f19e1e98fa5030f0a5c960afa7436657bc76f
beekaz <sooperfect00@gmail.com>
2023-11-15 12:46:25 -0800

    a class Square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,13 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const parentSquare = require('./5-square');
 
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
+module.exports = class Square extends parentSquare {
+  charPrint (c) {
+    if (typeof (c) === 'undefined') {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
   }
 };
